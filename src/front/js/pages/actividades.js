622e69fd95a7c7f9bd4d6fa4d6221d28769baf52
Serone25 <fran19842010@live.com>
2022-12-28 22:47:40 +0000

    arreglo guia.js. fallo en actividades.js

--- src/front/js/pages/actividades.js
+++ src/front/js/pages/actividades.js
@@ -10,28 +10,65 @@ export const Actividades = props => {
 	const { store, actions } = useContext(Context);
 	const params = useParams();
 
-	return (
-		<div className="container jumbotron body">
-			<h1 className="display-4">{store.actividades[params.theid].title}</h1>
-			<img src={rigoImageUrl} />
-			<h3>{store.actividades[params.theid].ciudad}</h3>
-			<h4>{store.actividades[params.theid].descripcion}</h4>
-			<h2>{store.actividades[params.theid].precio}</h2>
-			<h6>{store.actividades[params.theid].duracion}</h6>
-			<h3>Su guía:</h3>
-			<Link to="/guia/0">
-				<div className="card">
-				<img src="..." className="card-img-top" alt="..."/>
-				<div className="card-body">
-					<h5 className="card-title">{store.guia[0].nombre}</h5>
-				</div>
-							</div>
-						</Link>
-			<p>{store.comentarios[0].comentario}</p>
-			<p>{store.comentarios[1].comentario}</p>
-			<br></br>
-			<br></br>
+	const[guia, setGuia] = useState([])
+	const[actividades,setActividades] = useState([])
+	const [isLoading, setIsLoading] = useState(true);		//cargando guias
+	const [isLoading2, setIsLoading2] = useState(true);		//cargando actividades
 
+	useEffect(() => {
+		const promesaActividades = () => {
+			return new Promise((resolve, reject) => {
+				resolve(actions.dataFromAPI('/api/actividad/'+params.theid)) 
+			})
+		}
+		promesaActividades().then((datos) => { 
+			setActividades(datos)
+			setIsLoading2(false);
+		}
+		)
+		const promesaGuias = () => {
+			return new Promise((resolve, reject) => {
+				resolve(actions.dataFromAPI('/api/usuario/'+ actividades.id_guia)) 
+			})
+		}
+		promesaGuias().then((datos) => { 
+			setGuia(datos)
+			setIsLoading (false);
+		}
+		)
+	},[])
+
+	if (isLoading || isLoading2) {
+		return (
+		<div className="tbody">
+			<h1>Cargando...</h1>
+		</div>
+		)
+	}
+
+	return (
+		<div className="actividades-body">{console.log(actividades.id_guia)}
+			<div className="container jumbotron body">
+				<h1 className="display-4">{actividades.title}</h1>
+				<img src={rigoImageUrl} />
+				<h3>{actividades.ciudad}</h3>
+				<h4>{actividades.descripcion}</h4>
+				<h2>{actividades.precio}</h2>
+				<h6>{actividades.duracion}</h6>
+				<h3>Su guía:</h3>
+				<Link to={"/guia/"+ guia.id}>
+					<div className="card">
+						<img src="..." className="card-img-top" alt="..."/>
+						<div className="card-body">
+							<h5 className="card-title">{guia.nombre}</h5>
+						</div>
+					</div>
+				</Link>
+				<p>{store.comentarios[0].comentario}</p>
+				<p>{store.comentarios[1].comentario}</p>
+				<br></br>
+				<br></br>
+			</div>
 		</div>
 	);
 };
