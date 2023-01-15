dba2e99eb544c856b9710d51ab6e427b3e0ee188
Serone25 <fran19842010@live.com>
2023-01-15 20:59:52 +0000

    botones en footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -9,10 +9,38 @@ export const Footer = () => {
 		return fecha;
 	}
 	return(
-		<footer className="footer mt-auto py-4 text-center">
+		<footer className="footer mt-auto pt-3 pb-2 text-center">
 				<div className="row">
-					<div className="col">&copy;{anio()} OhMyTOWN.com</div>
-					<div className="col">Creado con <BsFillHeartFill/></div>
+					<div className="col footer_copyright">&copy;{anio()} OhMyTOWN.com</div>
+					<div className="col footer_iconos">
+						<ul className="icon-list">
+							<li className="icon-item">
+								<button className="icon-link">
+									<i className="fab fa-instagram"></i>
+								</button>
+							</li>
+							<li className="icon-item">
+								<button className="icon-link">
+									<i className="fab fa-facebook-f"></i>
+								</button>
+							</li>
+							<li className="icon-item">
+								<button className="icon-link">
+									<i className="fab fa-twitter"></i>
+								</button>
+							</li>
+							<li className="icon-item">
+								<button className="icon-link">
+									<i className="fab fa-youtube"></i>
+								</button>
+							</li>
+							<li className="icon-item">
+								<button className="icon-link">
+									<i className="fab fa-linkedin-in"></i>
+								</button>
+							</li>
+						</ul>
+					</div>
 				</div>
 			
 		</footer>
