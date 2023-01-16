63e13f91d9252b591eadb4603feece92ea0969e3
Don Pablo el de Felicitas <pabloalegrejuan@gmail.com>
2023-01-16 18:57:40 +0000

    error login finalizado

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -3,13 +3,13 @@ import { Context } from "../store/appContext";
 import "../../styles/login.css";
 import { Link } from "react-router-dom";
 
-import fondo2 from "../../img/fondo2.jpg"
-import {FaUserCircle} from "react-icons/fa";
+import fondo2 from "../../img/fondo2.jpg";
+import { FaUserCircle } from "react-icons/fa";
 
 import { useForm } from "react-hook-form"; // permite el manejo de formularios https://www.npmjs.com/package/react-hook-form
 
 export const Login = () => {
-  const { actions } = useContext(Context);
+  const { actions, store } = useContext(Context);
 
   const {
     register,
@@ -20,33 +20,48 @@ export const Login = () => {
   } = useForm(); // declaracion para react-hook-form
 
   let login = false;
-  const onSubmit = (data, e) => {
+  const onSubmit = async (data, e) => {
     e.preventDefault();
     const url = "/api/login";
     const method = "POST";
     const head = { "Content-Type": "application/json" };
     //console.log(email, password)
-    login = actions.solicitudesAPI(url, method, head, data);
-   
+    actions
+      .solicitudesAPI(url, method, head, data)
+      .then((el) => console.log("El resultado es;", el));
+    
   };
 
   return (
     <div className="login-body" style={{ backgroundImage: `url(${fondo2})` }}>
-      <div className="container login_espacio border border-white rounded px-0" style={{height:"400px",maxWidth:"400px",backgroundImage: `url(${fondo2})`}}>
-        <form className ="login_form" onSubmit={handleSubmit(onSubmit)}>
-          <h1 className="login_icon"><FaUserCircle color="white" fontSize="2.5em"/></h1>
+      <div
+        className="container login_espacio border border-white rounded px-0"
+        style={{
+          height: "400px",
+          maxWidth: "400px",
+          backgroundImage: `url(${fondo2})`,
+        }}
+      >
+        
+        <form className="login_form" onSubmit={handleSubmit(onSubmit)}>
+          <h1 className="login_icon">
+            <FaUserCircle color="white" fontSize="2.5em" />
+          </h1>
+          <div className="login_email">{store.message}</div>
           <div className="login_email">
             <i className="fa fa-user login_icono_email"></i>
-            <input  className ="login_input"
+            <input
+              className="login_input"
               type="text"
-              placeholder= "Email"
+              placeholder="Email"
               {...register("email", { required: true })} //crear el name del input y requerido react-hook-form
             />
           </div>
           <p></p>
           <div>
             <i className="fa fa-solid fa-lock login_icono_password"></i>
-            <input className ="login_input"
+            <input
+              className="login_input"
               autoComplete="off" //no permitir autocompletado del input
               type="text"
               placeholder="Password"
@@ -55,24 +70,27 @@ export const Login = () => {
           </div>
           <p></p>
           <div className="container login_button_body px-0">
-            <button className="login_button px-0" type="submit">Login</button>
+            <button className="login_button px-0" type="submit">
+              Login
+            </button>
           </div>
         </form>
       </div>
       <p></p>
       <div className="login_registro">
-        <p>Si no estás registrado pincha
+        <p>
+          Si no estás registrado pincha
           <Link to="/signup/">
             <span className="login_registro_link"> aquí</span>
           </Link>
         </p>
-        <p>Si no recuerdas tu contraseña pincha
+        <p>
+          Si no recuerdas tu contraseña pincha
           <Link to="/fpass/">
             <span className="login_registro_link"> aquí</span>
           </Link>
         </p>
       </div>
-      
     </div>
   );
 };
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,69 +1,65 @@
 import React, { useEffect, useState } from "react";
 
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			userid:false,
-			message: null,
-			
-		},
-		actions: {
-			dataFromAPI: async (url) => {
-				// para meter los datos de la API
-				const store = getStore()
-				if (url === '/logout') {
-					const token = localStorage.removeItem('jwt-token')
-					const userid = localStorage.removeItem('userid')
-					setStore({ userid: false })
-					return true
-				}
-					try {
-						const resp = await fetch(process.env.BACKEND_URL + url)
-						const data = await resp.json()
-						return data
-					} catch (error) {
-						return false
-					}
+  return {
+    store: {
+      userid: false,
+      message: null,
+    },
+    actions: {
+      dataFromAPI: async (url) => {
+        // para meter los datos de la API
+        const store = getStore();
+        if (url === "/logout") {
+          const token = localStorage.removeItem("jwt-token");
+          const userid = localStorage.removeItem("userid");
+          setStore({ userid: false });
+          return true;
+        }
+        try {
+          const resp = await fetch(process.env.BACKEND_URL + url);
+          const data = await resp.json();
+          return data;
+        } catch (error) {
+          return false;
+        }
+      },
 
-			},
+      solicitudesAPI: async (url, meth, head, bod) => {
+        const body = JSON.stringify(bod);
+        //console.log(body)
+        //console.log(url, meth, head, body);
+        const store = getStore();
 
-			solicitudesAPI: async (url, meth, head, bod) => {
-
-				const body = JSON.stringify(bod)
-				//console.log(body)
-				//console.log(url, meth, head, body);
-				const store = getStore()
-
-				await fetch(process.env.BACKEND_URL + url, {
-					method: meth,
-					headers: head,
-					body: body
-				}).then((resp) => resp.json()).then((data) => {
-					
-					if (data.token && url === '/api/login') {
-						localStorage.setItem("jwt-token", data.token)
-						localStorage.setItem("userid", data.userid)	
-						setStore({ userid: true })
-						if (store.userid){
-						window.location.href='/userhome'
-						}
-					
-						return true
-					}else{
-						return data
-					}		
-					
-
-				}).catch((error) => {
-					return 'Hubo un problema con la petición Fetch:' + error.message
-				})
-			},
-			logIn: () => {
-				setStore({ userid: true })
-				return true
-			},
-		}
-	};
+        await fetch(process.env.BACKEND_URL + url, {
+          method: meth,
+          headers: head,
+          body: body,
+        })
+          .then((resp) => resp.json())
+          .then((data) => {
+            if (data.token && url === "/api/login") {
+              localStorage.setItem("jwt-token", data.token);
+              localStorage.setItem("userid", data.userid);
+              setStore({ userid: true });
+              if (store.userid) {
+                setStore({ message: null });
+                window.location.href = "/userhome";
+              }
+            } else {
+              setStore({ message: "Error en el login" });
+            }
+          })
+          .catch((error) => {
+            return "Hubo un problema con la petición Fetch:" + error.message;
+          });
+      },
+      logIn: () => {
+        setStore({ userid: true });
+        return true;
+      },
+    },
+  };
 };
 
 export default getState;
