7ab8b850c6ecf345d1ba079999578fec55e1d324
Serone25 <fran19842010@live.com>
2023-01-17 20:01:26 +0000

    nuevo comentario maquetado

--- src/front/js/pages/nuevoComentario.js
+++ src/front/js/pages/nuevoComentario.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import { Link, useParams } from "react-router-dom";
 import { useForm } from "react-hook-form"; // permite el manejo de formularios https://www.npmjs.com/package/react-hook-form
 
-import "../../styles/signup.css";
+import "../../styles/nuevoComentario.css";
 import fondo2 from "../../img/fondo2.jpg";
 
 export const NuevoComentario = () => {
@@ -46,23 +46,21 @@ export const NuevoComentario = () => {
   };
 
   return (
-    <div className="signup-body" style={{ backgroundImage: `url(${fondo2})` }}>
+    <div className="nuevoComentario_body" style={{ backgroundImage: `url(${fondo2})` }}>
       <div
         className="container signup_espacio border border-white rounded px-0"
         style={{
-          height: "400px",
+          height: "auto",
           width: "400px",
           backgroundImage: `url(${fondo2})`,
         }}
       >
-        <form className="signup_form" onSubmit={handleSubmit(onSubmit)}>
-          <h2 className="sign_up_encabezado">NUEVO COMENTARIO</h2>
+        <form className="nuevoComentario_form" onSubmit={handleSubmit(onSubmit)}>
+          <h2 className="nuevoComentario_encabezado">NUEVO COMENTARIO</h2>
 
-          <div id="input_username" className="sign_up_email">
-            <i className="fas fa-envelope signup_icono_email"></i>
-            <input
-              className="signup_input"
-              type="textarea"
+          <div id="input_username" className="nuevoComentario_comentario">
+            <i className="fas fa-comment nuevoComentario_icono_comentario"></i>
+            <textarea className="ps-5 nuevoComentario_areatexto" rows="10" cols="40"
               {...register("texto", { required: true })} //crear el name del input y requerido react-hook-form
             />
           </div>
