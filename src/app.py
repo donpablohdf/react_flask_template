f9b4cef5f4a689c4939676ee6dfc75505994c6d3
Don Pablo <pabloalgrejuan@gmail.com>
2023-01-20 08:16:37 +0000

    arreglo py

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+from dotenv import load_dotenv
 from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
@@ -13,7 +14,7 @@ from api.admin import setup_admin
 from api.commands import setup_commands
 from flask_jwt_extended import JWTManager
 
-
+load_dotenv()
 ENV = os.getenv("FLASK_ENV")
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
@@ -21,10 +22,8 @@ app.url_map.strict_slashes = False
 
 # database configuration
 db_url = os.getenv("DATABASE_URL")
-if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db, compare_type = True)
@@ -32,10 +31,7 @@ db.init_app(app)
 
 # Configura la extensión Flask-JWT-Extended
 app.config["JWT_SECRET_KEY"] = "franpablodonato"
-#app.config["JWT_HEADER_TYPE"] = "Bearer"
-#app.config['PROPAGATE_EXCEPTIONS'] = True
 jwt = JWTManager(app)
-#jwt_m.init_app(app)
 
 # Allow CORS requests to this API
 CORS(app)
